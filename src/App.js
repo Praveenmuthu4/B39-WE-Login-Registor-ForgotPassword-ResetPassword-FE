d9ce1ecd097c61bef7bffb09b675571fc6929808
Praveenmuthu4 <praveenmuthu4@gmail.com>
2023-04-05 12:27:40 +0530

    removed Userpage since logout has created separately

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Login from './Pages/Login';
 import Logout from './Pages/Logout';
 import { Route, Routes, useNavigate } from "react-router-dom";
 import Register from './Pages/Register';
-import UserPage from './Pages/UserPage';
 import SuccessMessage from './Pages/successMessage';
 import ResetPassword from './Pages/Reset';
 import ForgotPassword from './Pages/ForgotPassword';
@@ -19,7 +18,6 @@ function App() {
       <Routes>
       <Route path="/" element={<Dashboard />}></Route>
               <Route path="/login" element={<Login />}></Route>
-              <Route path="/userDashboard" element={<UserPage />}></Route>
               <Route path="/logout" element={<Logout />}></Route>
               <Route path="/register" element={<Register/>}></Route>
               <Route path="/userCreated" element={<SuccessMessage/>}></Route>
--- src/Pages/Logout.js
+++ src/Pages/Logout.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import './logout.css'
+import { Button } from "@mui/material";
 
 export default function Logout() {
   const navigate = useNavigate();
 
   return (
     <div className="whole-logout-container">
-      <button type="submit" onClick={() => navigate("/")} className='logout-btn'>
-        Logout
-      </button>
+      <h2> Hello Username </h2>
+      <h3>Welcome back,</h3>
+      <div>
+        <Button onClick={()=>navigate('/')} className='logout-btn'>
+            Logout
+        </Button>
+      </div>
     </div>
   );
 }
