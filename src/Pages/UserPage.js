ead1c70cfe843e643f935b9c044ba19a0ced2021
Praveenmuthu4 <praveenmuthu4@gmail.com>
2023-04-05 12:24:43 +0530

    changed useState in UserPage

--- src/Pages/UserPage.js
+++ src/Pages/UserPage.js
@@ -1,49 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import { Button } from '@mui/material';
+import React from 'react'
 import { useNavigate } from 'react-router-dom';
-import {API} from '../global';
+
 
 export default function UserPage() {
-  const [data, setData] = useState(false);
-    const history = useNavigate();
-
-    const DashboardValid = async () => {
-      let token = localStorage.getItem("usersdatatoken");
-  
-      const res = await fetch(`${API}/validuser`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": token
-        }
-      });
-  
-      const data = await res.json();
-  
-      if (data.status == 401 || !data) {
-        console.log("user not valid");
-      } else {
-        console.log("user verify");
-        setLoginData(data)
-        history("/");
-      }
-    }
-  
-    useEffect(() => {
-      setTimeout(()=>{
-        DashboardValid();
-        setData(true)
-      },2000)
-  
-    }, [])
+ const navigate = useNavigate();
 
   return (
     <div>
-      <p> Hello Username </p>
-      <p>Welcome back,</p>
+      <h2> Hello Username </h2>
+      <h3>Welcome back,</h3>
       <div>
-        <button onClick={()=>history('/')}>
+        <Button onClick={()=>navigate('/')}>
             Logout
-        </button>
+        </Button>
       </div>
     </div>
   )
